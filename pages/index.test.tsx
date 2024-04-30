4768bde0505fe0b44f1569f8dc4d1aca0a2f1c0b
Chris <lukaizhi@chriss-air.mynetworksettings.com>
2024-04-30 00:05:08 -0400

    uncommented and fixed failing test

--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import { render, screen, act } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Home from "@/pages/index";
 
 describe("Home", () => {
   it("renders a heading", () => {
-    // // Wrap the rendering of your component in the act function
-    
-    //   render(<Home />);
+    render(<Home />);
  
+    // Perform assertions after the component has been rendered
+    const heading = screen.getByRole("heading", {
+      name: /Side React Take-home Assignment/i,
+    });
 
-    // // Perform assertions after the component has been rendered
-    // const heading = screen.getByRole("heading", {
-    //   name: /Side React Take-home Assignment/i,
-    // });
-
-    // expect(heading).toBeInTheDocument();
+    expect(heading).toBeInTheDocument();
   });
 });
