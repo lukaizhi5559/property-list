076cbfcb4819a7f9b3f110485da6772a9ccc99b9
Chris <lukaizhi@chriss-air.mynetworksettings.com>
2024-04-30 09:22:37 -0400

    Added a no results found message if properties are out of view on the map

--- components/PropertyList/index.tsx
+++ components/PropertyList/index.tsx
@@ -38,39 +38,43 @@ const PropertyList: React.FC<Props> = (
   }
 
   return (
-    extractedPropertyData.map((property) => (
-      <div 
-        key={property.mlsId} 
-        id={`property-${property.mlsId}`}
-        data-testid={`property-${property.mlsId}`}
-        className={`${styles.property} ${selectedPropertyMlsId === property.mlsId && mapVisible ? styles.highlighted : ''}`}
-        onClick={() => handlePropertyCardClicked(property)}
-      >
-        <div className={styles.imageContainer}>
-          <img className={mapVisible ? '' : styles.imgOverride} src={property.photos[0]} alt="Property" />
-          <div className={styles.details}>
-            <p>
-              {property.bedrooms} BR | {calculateTotalBaths(property.bathsFull, property.bathsHalf)} Bath | {property.area} Sq Ft
-            </p>
-            <h2>${property.listPrice.toLocaleString()}</h2>
-            <p>{property.address.streetName}, {property.address.city}, {getStateAbbreviation(property.address.state)}</p>
-            <p>Listed: {formatDate(property.listDate)}</p>
-          </div>
-          <div className={styles.favoriteButton} onClick={(e) => {
-            e.stopPropagation();
-            e.preventDefault();
+    extractedPropertyData.length > 0 ? (
+      extractedPropertyData.map((property) => (
+        <div 
+          key={property.mlsId} 
+          id={`property-${property.mlsId}`}
+          data-testid={`property-${property.mlsId}`}
+          className={`${styles.property} ${selectedPropertyMlsId === property.mlsId && mapVisible ? styles.highlighted : ''}`}
+          onClick={() => handlePropertyCardClicked(property)}
+        >
+          <div className={styles.imageContainer}>
+            <img className={mapVisible ? '' : styles.imgOverride} src={property.photos[0]} alt="Property" />
+            <div className={styles.details}>
+              <p>
+                {property.bedrooms} BR | {calculateTotalBaths(property.bathsFull, property.bathsHalf)} Bath | {property.area} Sq Ft
+              </p>
+              <h2>${property.listPrice.toLocaleString()}</h2>
+              <p>{property.address.streetName}, {property.address.city}, {getStateAbbreviation(property.address.state)}</p>
+              <p>Listed: {formatDate(property.listDate)}</p>
+            </div>
+            <div className={styles.favoriteButton} onClick={(e) => {
+              e.stopPropagation();
+              e.preventDefault();
 
-            onToggleFavorite(property.mlsId, true);
-          }}>
-            {favorites.includes(property.mlsId) ? (
-              <img src="/heart-fill.svg" alt="Favorite" />
-            ) : (
-              <img src="/heart-stroke.svg" alt="Not Favorite" />
-            )}
+              onToggleFavorite(property.mlsId, true);
+            }}>
+              {favorites.includes(property.mlsId) ? (
+                <img src="/heart-fill.svg" alt="Favorite" />
+              ) : (
+                <img src="/heart-stroke.svg" alt="Not Favorite" />
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    ))
+      ))
+    ) : (
+      <div className={styles.noResults}>No results found</div>
+    )
   );
 };
 
